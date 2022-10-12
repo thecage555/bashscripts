e29c63b9eb654be8b285dae893305b2a963c584e
thecage555 <113894844+thecage555@users.noreply.github.com>
2022-10-12 11:11:04 +0200

    Update apachescript.sh

--- apachescript.sh
+++ apachescript.sh
@@ -4,6 +4,7 @@ sudo apt -y update
 sudo apt -y upgrade
 
 sudo apt -y install apache2
+sudo apt -y install mysql
 sudo apt -y install mysql-server
 sudo apt -y install php libapache2-mod-php php-mysql
 sudo apt -y install phpmyadmin
@@ -14,9 +15,11 @@ sudo echo "<IfModule mod_dir.c>
 
 sudo systemctl restart apache2
 
-sudo mkdir /var/www/printer
-sudo chown -R $USER:$USER /var/www/printer
-sudo chmod -R 755 /var/www/printer
+sudo chown -R www-data:www-data /var/www/192.168.0.5
+sudo chmod -R 755 /var/www/192.168.0.5
+sudo chmod -R g+w /var/www/192.168.0.5
+sudo usermod -a -G www-data $USER
+#adding user to correct group www-data
 
 sudo echo "<html>
     <head>
@@ -25,21 +28,23 @@ sudo echo "<html>
     <body>
         <h1>Success!  The printer server block is working and so is the bash script!</h1>
     </body>
-</html>" > /var/www/printer/index.html
+</html>" > /var/www/192.168.0.5/index.html
 
 sudo echo "<VirtualHost *:80>
     ServerAdmin webmaster@haahrs
-    ServerName printer
-    ServerAlias www.printer
-    DocumentRoot /var/www/printer
+    ServerName 192.168.0.5
+    ServerAlias www.192.168.0.5/
+    DocumentRoot /var/www/192.168.0.5
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>" > /etc/apache2/sites-available/printer.conf
+</VirtualHost>" > /etc/apache2/sites-available/192.168.0.5.conf
 
-sudo a2ensite printer.conf
+sudo a2ensite 192.168.0.5.conf
 sudo a2dissite 000-default.conf
-sudo a2dissite 192.168.0.5.conf
 
-sudo ln -s /usr/share/phpmyadmin /var/www/printer
+
+sudo ln -s /usr/share/phpmyadmin /var/www/192.168.0.5
 
 sudo systemctl restart apache2
+
+echo "you may need to run the script again after adding php files/folders or use usermod -a -G www-data $USER again"
