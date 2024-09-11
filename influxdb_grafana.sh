145a5045b08c1563145dacdcfc4a790dbffa9664
Emil Hansen <113894844+thecage555@users.noreply.github.com>
2024-09-11 11:20:04 +0200

    Add files via upload
    adding repos and installing InfluxDBv2 and Grafana

---
+++ influxdb_grafana.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# run from a home dir
+
+
+curl --silent --location -O \
+https://repos.influxdata.com/influxdata-archive.key
+echo "943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515  influxdata-archive.key" \
+| sha256sum --check - && cat influxdata-archive.key \
+| gpg --dearmor \
+| tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null \
+&& echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' \
+| tee /etc/apt/sources.list.d/influxdata.list
+# Install influxdb
+sudo apt-get update && sudo apt-get install influxdb2
+
+sudo systemctl unmask influxdb
+sudo systemctl enable influxdb
+sudo systemctl start influxdb
+
+
+
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor > /etc/apt/keyrings/grafana.gpg
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update 
+
+sudo apt install -y grafana
+
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
